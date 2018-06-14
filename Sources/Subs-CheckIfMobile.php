2ef49af1047d933161e198c928570cc5e6e038fb
Robert Israel <robert.israel@teamaol.com>
2018-06-13 20:16:15 -0400

    Further cleanup.

--- Sources/Subs-CheckIfMobile.php
+++ Sources/Subs-CheckIfMobile.php
@@ -18,15 +18,24 @@
  */
 function CheckIfMobile()
 {
+	//Gets set so if you want to use mobile status for other
+	//settings, it's available without reparsing.
 	global $context;
 
-	# Skip all the parsing if we already know it's mobile
+	//Skip all the parsing if we already know it's mobile
 	if(isset($context['MobileDevice']))
 	{
 		return true;
 	}
 
-	$useragent = isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
+	//if we don't have a useragent, assume we're not mobile?
+	if(isset($_SERVER['HTTP_USER_AGENT'])
+	{
+		$useragent = strtolower($_SERVER['HTTP_USER_AGENT']);
+	} else {
+		return false;
+	}
+	
 	$context['MobileDevice'] = array(
 		'isMobile' => false,
 	);
@@ -35,37 +44,41 @@ function CheckIfMobile()
 	$genericStrings = array(
 		'engineWebKit' 		=> 'webkit',
 		'deviceMacPpc' 		=> 'macintosh', //Used for disambiguation
-		'deviceWindows' 	=> 'windows',
+		'vendorMS'		=> 'microsoft',
+		'osWindows' 		=> 'windows',
 		'devicePpc' 		=> 'ppc', 	//Stands for PocketPC
-		'linux' 		=> 'linux',
+		'osLinux' 		=> 'linux',
 		'engineOpera' 		=> 'opera', 	//Popular browser
-		'deviceTablet' 		=> 'tablet', 	//Generic term for slate and tablet devices
 		'engineMozilla' 	=> 'mozilla', 
+		'engineGecko' 		=> 'gecko', 
+		'engineChrome' 		=> 'chrome', 
+		'engineSafari' 		=> 'safari', 
 	);
 
 	//Initialize some initial smartphone string variables.
 	$mobileStrings = array(
+		//Apple
 		'deviceIphone' 		=> 'iphone',
 		'deviceIpod' 		=> 'ipod',
 		'deviceIpad' 		=> 'ipad',
-
+		//Google
 		'deviceAndroid' 	=> 'android',
 		'deviceGoogleTV' 	=> 'googletv',
 		'deviceXoom' 		=> 'xoom', 	//Motorola Xoom
 		'deviceHtcFlyer' 	=> 'htc_flyer', //HTC Flyer
 		'deviceNuvifone' 	=> 'nuvifone',  //Garmin Nuvifone
 		'deviceGTI9000' 	=> 'gt-i9000',  //Samsung Galaxy I9000
-
+		//Symbian
 		'deviceSymbian' 	=> 'symbian',
 		'deviceS60' 		=> 'series60',
 		'deviceS70' 		=> 'series70',
 		'deviceS80' 		=> 'series80',
 		'deviceS90' 		=> 'series90',
-
+		//Microsoft
 		'deviceWinPhone7' 	=> 'windows phone os 7',
 		'deviceWinMob' 		=> 'windows ce',
 		'enginePie' 		=> 'wm5 pie', 		//An old Windows Mobile
-
+		//RIM
 		'deviceBB' 		=> 'blackberry',
 		'vendorRIM' 		=> 'vnd.rim', 		//Detectable when BB devices emulate IE or Firefox
 		'deviceBBStorm' 	=> 'blackberry95',  	//Storm 1 and 2
@@ -75,24 +88,21 @@ function CheckIfMobile()
 		'deviceBBCurve' 	=> 'blackberry89', 	//Curve2
 		'deviceBBTorch' 	=> 'blackberry 98', 	//Torch
 		'deviceBBPlaybook' 	=> 'playbook', 		//PlayBook tablet
-
+		//Palm
 		'devicePalm' 		=> 'palm',
 		'deviceWebOS' 		=> 'webos', 		//For Palm's line of WebOS devices
 		'deviceWebOShp' 	=> 'hpwos', 		//For HP's line of WebOS devices
-
 		'engineBlazer' 		=> 'blazer', 		//Old Palm browser
 		'engineXiino' 		=> 'xiino', 		//Another old Palm
-
+		//Amazon
 		'deviceKindle' 		=> 'kindle', 		//Amazon Kindle, eInk one.
-
+		//Mozilla
 		'engineFirefoxIOS' 	=> 'fxios', 		//Mozilla FF for IOS
-
 		//Initialize variables for mobile-specific content.
 		'vndwap' 		=> 'vnd.wap',
 		'wml' 			=> 'wml',
-
 		//Initialize variables for other random devices and mobile browsers.
-		'deviceTablet' 		=> 'tablet', 		//Generic term for slate and tablet devices
+		//It might be cheeky to call consoles 'mobile', but we do.
 		'deviceBrew' 		=> 'brew',
 		'deviceDanger' 		=> 'danger',
 		'deviceHiptop' 		=> 'hiptop',
@@ -108,28 +118,25 @@ function CheckIfMobile()
 		'deviceMidp' 		=> 'midp', 		//a mobile Java technology
 		'uplink' 		=> 'up.link',
 		'engineTelecaQ' 	=> 'teleca q', 		//a modern feature phone browser
-		'vendorXiaomi' 		=> 'miui',        	//popular Chinese webkit browser
-		'vendorUCWeb' 		=> 'ucbrowser',   	//another Chinese webkit browser
+		'vendorXiaomi' 		=> 'miui',        	//popular Chinese mobile browser
+		'vendorUCWeb' 		=> 'ucbrowser',   	//another Chinese mobile browser
 		'vendorCloudMosa'	=> 'puffin', 		//Puffin Android optimized browser
-
 		//Generics
 		'devicePda' 	=> 'pda', 	//some devices report themselves as PDAs
 		'mini' 		=> 'mini',  	//Some mobile browsers put 'mini' in their names.
 		'mobile' 	=> 'mobile', 	//Some mobile browsers put 'mobile' in their user agent strings.
 		'mobi' 		=> 'mobi', 	//Some mobile browsers put 'mobi' in their user agent strings.
-
+		'tablet' 	=> 'tablet', 	//Some mobile browsers put 'tablet' in their user agent strings.
 		//Nokia's Internet Tablets.
 		'maemo' 	=> 'maemo',
 		'qtembedded' 	=> 'qt embedded', 	//for Sony Mylo and others
 		'mylocom2' 	=> 'com2', 		//for Sony Mylo also
-
 		//In some UserAgents, the only clue is the manufacturer.
 		'manuSonyEricsson' 	=> 'sonyericsson',
 		'manuericsson'		=> 'ericsson',
 		'manuSamsung1' 		=> 'sec-sgh',
 		'manuSony' 		=> 'sony',
 		'manuHtc' 		=> 'htc', 	//Popular Android and WinMo manufacturer
-
 		//In some UserAgents, the only clue is the operator.
 		'svcDocomo' 		=> 'docomo',
 		'svcKddi' 		=> 'kddi',
